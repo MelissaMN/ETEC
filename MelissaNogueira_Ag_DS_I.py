eb0de90e00ecc3d770c03806c023f70f626bcd66
Melissa Nogueira <melissamarconde@gmail.com>
2026-04-09 20:49:56 -0300

    Removendo arquivo definitivamente

--- MelissaNogueira_Ag_DS_I.py
+++
@@ -1,9 +0,0 @@
-# Entrada das informações
-produto = input("Digite o nome do produto: ")
-preco = float(input("Digite o preço unitário (ex.: 4.50): "))
-quantidade = int(input("Digite a quantidade comprada: "))
-# Processamento (realização do cálculo)
-total = preco * quantidade
-# Saída – Exibindo as informações.
-print(f"Você comprou {quantidade}x {produto}(s).")
-print(f"Valor total da compra: R$ {total:.2f}")
